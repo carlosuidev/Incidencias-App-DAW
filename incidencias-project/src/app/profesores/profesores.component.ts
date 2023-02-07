04761237b17cfdf30439b93cf9c1c947aeace6ac
Carlos Almendros <77893059+CarlosUIUX@users.noreply.github.com>
2023-02-07 22:47:45 +0100

    7/2

---
+++ incidencias-project/src/app/profesores/profesores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "profesores",
+  templateUrl: "./profesores.component.html",
+  styleUrls: ["./profesores.component.css"]
+})
+
+export class ProfesoresComponent{
+
+}
