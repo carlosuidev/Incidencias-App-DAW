df0c965062f0aa05efa535e66eb2892fe441a0c2
Carlos Almendros <77893059+CarlosUIUX@users.noreply.github.com>
2023-02-18 12:06:21 +0100

    18/2
    Diseño de botones y ajustes de la barra lateral

---
+++ src/js/redirecciones.js
@@ -0,0 +1,24 @@
+addEventListener('DOMContentLoaded', eventos);
+
+function eventos() {
+    //document.getElementById("").addEventListener("click", irNuevaIncidencia);
+    //document.getElementById("").addEventListener("click", irIncidenciasActivas);
+    //document.getElementById("").addEventListener("click", irHistorial);
+    document.getElementById("educamadridIncidencias").addEventListener("click", irEducamadrid);
+}
+
+function irNuevaIncidencia() {
+    window.location.href = "www.yoursite.com";
+}
+
+function irIncidenciasActivas() {
+    window.location.href = "www.yoursite.com";
+}
+
+function irHistorial() {
+    window.location.href = "www.yoursite.com";
+}
+
+function irEducamadrid() {
+    window.location.href = "https://aulavirtual33.educa.madrid.org/ies.piobaroja.madrid/";
+}
